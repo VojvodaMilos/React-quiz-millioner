5d515d4b21ff668423b572314c1c57804d0278fc
Milos1988 <vojvoda1988>
2022-09-29 13:49:25 +0200

    prvi dan-Css

---
+++ src/component/Trivia.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Trivia = () => {
+  return (
+    <div className="trivia">
+      <div className="question">What's the best youtube channel?</div>
+      <div className="answers">
+        <div className="answer">Lama Dew</div>
+        <div className="answer">Lama Dew</div>
+        <div className="answer">Lama Dew</div>
+        <div className="answer">Lama Dew</div>
+      </div>
+    </div>
+  );
+};
+
+export default Trivia;
