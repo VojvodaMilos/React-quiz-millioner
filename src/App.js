1bd7cdf3dc73d0dd1e670e9b7d0445ae53dcced2
Milos1988 <vojvoda1988>
2022-10-04 13:42:33 +0200

    sound

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import Trivia from "./component/Trivia";
+import Timer from "./Timer";
 
 function App() {
   const [questionNumber, setQuestionNumber] = useState(1);
@@ -110,7 +111,9 @@ function App() {
         ) : (
           <>
             <div className="top">
-              <div className="timer">30</div>
+              <div className="timer">
+                <Timer setStop={setStop} questionNumber={questionNumber} />
+              </div>
             </div>
             <div className="bottom">
               <Trivia
--- src/component/Trivia.js
+++ src/component/Trivia.js
@@ -1,9 +1,21 @@
 import React, { useEffect, useState } from "react";
+import useSound from "use-sound";
+import play from "../sounds/src_sounds_play.mp3";
+import correct from "../sounds/src_sounds_correct.mp3";
+import wrong from "../sounds/src_sounds_wrong.mp3";
 
 const Trivia = ({ data, questionNumber, setQuestionNumber, setStop }) => {
   const [question, setQuestion] = useState(null);
   const [selectAnswers, setSelectAnswers] = useState(null);
   const [className, setClassName] = useState("answer");
+  const [letsPlay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
+
+  useEffect(() => {
+    console.log("kreni");
+    letsPlay();
+  }, [letsPlay]);
 
   useEffect(() => {
     setQuestion(data[questionNumber - 1]);
@@ -35,10 +47,11 @@ const Trivia = ({ data, questionNumber, setQuestionNumber, setStop }) => {
     <div className="trivia">
       <div className="question">{question?.question}</div>
       <div className="answers">
-        {question?.answers.map((a) => (
+        {question?.answers.map((a, id) => (
           <div
             className={selectAnswers === a ? className : "answer"}
             onClick={() => handleClick(a)}
+            key={id}
           >
             {a.text}
           </div>
