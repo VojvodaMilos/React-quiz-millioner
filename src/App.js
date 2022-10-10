18f54dfd920dcec356e930abef1ead8272716503
Milos1988 <vojvoda1988>
2022-10-10 13:42:22 +0200

    ostala piramida

--- src/App.js
+++ src/App.js
@@ -1,17 +1,136 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
-// import Trivia from "./component/Trivia";
+import Trivia from "./component/Trivia";
 
 function App() {
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [stop, setStop] = useState(false);
+
+  const data = [
+    {
+      id: 1,
+      question: "Rolexis the company that specializes in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "When did the website `Facebook` launch?",
+      answers: [
+        {
+          text: 2004,
+          correct: true,
+        },
+        {
+          text: 2005,
+          correct: false,
+        },
+        {
+          text: 2006,
+          correct: false,
+        },
+        {
+          text: 2007,
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who played the character of harry potter in movie",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Lionardo di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washinton",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+  ];
+
+  const moneyPyramid = [
+    { id: 1, amount: "$ 100" },
+    { id: 2, amount: "$ 200" },
+    { id: 3, amount: "$ 300" },
+    { id: 4, amount: "$ 500" },
+    { id: 5, amount: "$ 1000" },
+    { id: 6, amount: "$ 2000" },
+    { id: 7, amount: "$ 4000" },
+    { id: 8, amount: "$ 8000" },
+    { id: 9, amount: "$ 16000" },
+    { id: 10, amount: "$ 32000" },
+    { id: 11, amount: "$ 64000" },
+    { id: 12, amount: "$ 125000" },
+    { id: 13, amount: "$ 250000" },
+    { id: 14, amount: "$ 500000" },
+    { id: 15, amount: "$ 1000000" },
+  ].reverse();
+
   return (
     <div className="app">
-      <div className="main">
-        <div className="top"></div>
-        <div className="bottom"></div>
-      </div>
-      <div className="pyramid">
-        <ul className="moneyList"></ul>
-      </div>
+      {stop ? (
+        <h1 className="endText">You earned:{}</h1>
+      ) : (
+        <>
+          <div className="main">
+            <div className="top"></div>
+            <div className="bottom">
+              <Trivia
+                data={data}
+                questionNumber={questionNumber}
+                setQuestionNumber={setQuestionNumber}
+                setStop={setStop}
+              />
+            </div>
+          </div>
+          <div className="pyramid">
+            <ul className="moneyList">
+              {moneyPyramid.map((m) => {
+                return (
+                  <li
+                    key={m.id}
+                    className={
+                      questionNumber == m.id
+                        ? "moneyListItem active"
+                        : "moneyListItem"
+                    }
+                  >
+                    <span className="moneyListItemNumber">{m.id}</span>{" "}
+                    <span className="moneyListItemAmount">{m.amount}</span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/component/Trivia.js
+++ src/component/Trivia.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Trivia = () => {
+const Trivia = ({ data, questionNumber, setQuestionNumber, setStop }) => {
+  const [selectAnswers, setSelectAnswers] = useState(null);
+  const [className, setClassName] = useState("answer");
+
+  // const delay = () =>;
+
+  const handleClick = (answer) => {
+    setSelectAnswers(answer);
+    setClassName("answer active");
+    if (answer.correct) {
+      setClassName("answer correct");
+      setTimeout(() => {
+        setQuestionNumber((prev) => prev + 1);
+      }, 3000);
+    } else {
+      setClassName("answer wrong");
+      setTimeout(() => {
+        setStop(true);
+      }, 3000);
+    }
+  };
+
   return (
     <div className="trivia">
-      <div className="question">What's the best youtube channel?</div>
+      <div className="question">{data[questionNumber - 1].question}</div>
       <div className="answers">
-        <div className="answer">Lama Dew</div>
-        <div className="answer">Lama Dew</div>
-        <div className="answer">Lama Dew</div>
-        <div className="answer">Lama Dew</div>
+        {data[questionNumber - 1].answers.map((a, id) => {
+          return (
+            <div
+              onClick={() => {
+                handleClick(a);
+              }}
+              key={id}
+              className={selectAnswers === a ? className : "answer"}
+            >
+              {a.text}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
